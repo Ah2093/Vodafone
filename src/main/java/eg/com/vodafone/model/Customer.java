578817fe10dd4de37215e16e290a7c6cc44faf85
ahmed hamdi <ah2093@fayoum.edu.eg>
2025-02-01 16:02:12 +0200

    creating model object

---
+++ src/main/java/eg/com/vodafone/model/Customer.java
@@ -0,0 +1,28 @@
+package eg.com.vodafone.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    private String userName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    private String phonenumber;
+
+}
